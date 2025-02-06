98887915317c8b8bc07ebaaadde7e207fa5eb1b9
Baptiste Jouteau <baptistejouteau@gmail.com>
2025-02-06 11:05:27 +0100

    Inscription

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,7 +3,8 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/connexion", "Connexion", "/pages/connexion.html")];
+    new Route("/connexion", "Connexion", "/pages/connexion.html"),
+    new Route("/inscription", "inscription", "/pages/inscription.html"),];
     
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Ecoride";
