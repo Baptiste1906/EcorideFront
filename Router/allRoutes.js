a09fd17f1e5651c7db6c8dd96ad043329d1d9eb9
Baptiste Jouteau <baptistejouteau@gmail.com>
2025-02-15 14:52:52 +0100

    authentification

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,7 +4,12 @@ import Route from "./Route.js";
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
     new Route("/connexion", "Connexion", "/pages/connexion.html"),
-    new Route("/inscription", "inscription", "/pages/inscription.html"),];
+    new Route("/inscription", "inscription", "/pages/inscription.html", "/js/inscription.js"),
+    new Route("/moncompte", "Mon compte", "/pages/moncompte.html"),
+    new Route("/covoiturage", "Covoiturage", "/pages/covoiturage.html"),
+    new Route("/changermdp", "Changer mot de passe", "/pages/changermdp.html"),
+
+];
     
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Ecoride";
